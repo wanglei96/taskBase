31cb9712ba056ec03a2d1285b698ddbc180efce1
wanglei96 <1036697284@qq.com>
2021-06-19 22:42:51 +0800

    feat:新增模块二阶段一作业

---
+++ class_2_1_task/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+ * @description: **
+ * @date: 2021/6/19
+ * @version: 1.0
+ * @fileName: __init__.py
+ * @author: 'WangLei'
+"""
+
+
+
---
+++ class_2_1_task/hero_factory.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+ * @description: 英雄工厂
+ * @date: 2021/6/19
+ * @version: 1.0
+ * @fileName: hero_factory.py
+ * @author: 'WangLei'
+"""
+from class_2_1_task.hero import *
+
+
+class HeroFactory(object):
+    def create_hero(self, name):
+        # 英雄字典
+        dict_hero = {
+            'Timo': Timo,
+            'Jinx': Jinx
+        }
+        # 根据英雄名获取英雄类
+        hero_model = dict_hero.get(name)
+        if hero_model:
+            return dict_hero.get(name)()
+        else:
+            raise Exception('没有找到英雄，请先创建英雄类')
+
+
+if __name__ == "__main__":
+    hero_factory = HeroFactory()
+    hero_timo = hero_factory.create_hero('Timo')
+    hero_jinx = hero_factory.create_hero('Jinx')
+    hero_timo.fight(hero_jinx.hp, hero_jinx.power)
+
+
+
